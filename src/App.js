397845c0fd5e526e41bb1bb1c5254b962cf1be87
Nayab-Sarfaraj <nayabsarfaraj@gmail.com>
2024-01-12 23:05:54 +0530

    version one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [counter,setCounter]=useState(0)
+const increment=()=>{
+ if(counter<Number.MAX_VALUE)
+  setCounter(counter+1)
+}
+const decrement=()=>{
+  if(counter>0)
+  setCounter(counter-1)
+}
+const reset=()=>{
+  setCounter(0)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-900 h-screen w-screen flex items-center justify-center text-[azure] ">
+      <div>
+        <h1 className=" text-6xl uppercase">Counter {counter}</h1>
+        <div className="flex  items-center gap-5 mt-5  justify-center">
+          <button className="bg-black px-2 py-1 rounded-md  hover:bg-slate-500 hover:text-black " onClick={increment}>Increment</button>
+          <button className="bg-black px-2 py-1 rounded-md hover:bg-slate-500 hover:text-black " onClick={decrement}>Decrement</button>
+        </div>
+        <div className="text-center mt-5">
+        <button className="bg-black px-2 py-1 rounded-md hover:bg-slate-500 hover:text-black " onClick={reset}>Reset</button>
+        </div>
+       
+      </div>
     </div>
   );
 }
